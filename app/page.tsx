a55cdb66d06811d591e03d98f5044d94aba76ca5
Shoto <shinbullon@gmail.com>
2025-09-17 22:12:02 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { connection } from "next/server";
 import { CatImage } from "./cat-image";
 import { fetchImage } from "./fetch-image";
 
 export default async function Home() {
+
+  await connection();
 
   const image = await fetchImage();
 
